407941d2306e1b29b954c0ad6349412457c5fa1e
rupamsutar <rupamsutar281998@gmail.com>
2025-04-02 22:51:49 +0530

    Fetching data by leveraging NextJS and fullstack capabilities.

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -2,13 +2,14 @@ import MealItem from "./meal-item";
 import classes from "./meals-grid.module.css";
 
 export default function MealsGrid({meals}) {
+    
     return (
         <ul>
             {
-                meals.map((meal) => {
-                    <li key={meal.id}>
-                        <MealItem {...meal} />
-                    </li>
+                meals.map((meal) => {                    
+                    return <li key={meal.id}>
+                    <MealItem {...meal} />
+                </li>
                 })
             }
         </ul>
