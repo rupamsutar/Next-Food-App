690ad10ededa213a71d94255e9c555a2d6a8c604
rupamsutar <rupamsutar281998@gmail.com>
2025-04-03 22:43:12 +0530

    Added validations.

---
+++ app/meals/share/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function ErrorMessage() {
+    return(
+        <main className="error">
+            <h1>Submitting failed</h1>
+            <p>Failed to create meal data, please try again later</p>
+        </main>
+    )
+}
