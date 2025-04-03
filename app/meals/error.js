e2a3845f05ee48532ef1df505ab776f0ec311e53
rupamsutar <rupamsutar281998@gmail.com>
2025-04-03 15:43:02 +0530

    Handling Errors and Not found, Loading & Rendering the meals via Dynamic route.

---
+++ app/meals/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function ErrorMessage() {
+    return(
+        <main className="error">
+            <h1>An Error occured</h1>
+            <p>Failed to fetch meal data, please try again later</p>
+        </main>
+    )
+}
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return(
+        <main className="not-found">
+            <h1>Not Found</h1>
+            <p>Cannot navigate to such page</p>
+        </main>
+    )
+}
